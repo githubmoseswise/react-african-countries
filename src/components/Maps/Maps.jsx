0273fe06a67d61a8b2d7830c058419cc15271a6c
moseswisegit <moseswisegit@gmail.com>
2024-08-02 17:22:00 +0000

    Add marker svg

--- src/components/Maps/Maps.jsx
+++ src/components/Maps/Maps.jsx
@@ -3,10 +3,21 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import s from "./style.module.css";
 
+// Importation de l'image de marqueur
+import markerIcon from "../../assets/images/marker.svg";
+
 function Maps({ lat, lon, name }) {
   useEffect(() => {
+    // Crée une icône personnalisée pour le marqueur
+    const customIcon = L.icon({
+      iconUrl: markerIcon,
+      iconSize: [32, 32], // Taille de l'icône
+      iconAnchor: [16, 32], // Point de l'icône qui sera positionné sur les coordonnées
+      popupAnchor: [0, -32], // Point à partir duquel le popup apparaîtra
+    });
+
     // Initialise la carte
-    const map = L.map("map").setView([lat, lon], 5);
+    const map = L.map("map").setView([lat, lon], 4);
 
     // Ajoute un fond de carte
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -15,24 +26,13 @@ function Maps({ lat, lon, name }) {
     }).addTo(map);
 
     // Ajouter un marqueur
-    L.marker([lat, lon])
+    L.marker([lat, lon], { icon: customIcon })
       .addTo(map)
       .bindPopup(name)
       .openPopup();
 
-    // Définir les coordonnées pour la surbrillance
-    const bounds = [
-      [lat , lon ], // Coordonnée sud-ouest
-      [lat , lon ], // Coordonnée nord-est
-    ];
-
-    // Ajouter une surbrillance avec un polygone
-    L.polygon(bounds, {
-      color: 'red', // Couleur de la bordure
-      weight: 2,    // Épaisseur de la bordure
-      fillColor: 'red', // Couleur de remplissage
-      fillOpacity: 0.2 // Opacité du remplissage
-    }).addTo(map);
+    // Ajuste la vue de la carte pour centrer le marqueur
+    map.setView([lat, lon], 10); // Ajuster le niveau de zoom si nécessaire
 
     // Nettoyage de la carte lorsque le composant est démonté
     return () => {
